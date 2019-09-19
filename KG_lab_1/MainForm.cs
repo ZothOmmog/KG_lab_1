2569dae08b132ada8696e830e5a1eabc204baba4
ZothOmmog <paklin.102@yandex.ru>
2019-09-19 15:09:31 +0500

    1. Линии хранятся в массиве. 2. Возможность добавлять линию на холст и в массив.

--- KG_lab_1/MainForm.cs
+++ KG_lab_1/MainForm.cs
@@ -12,21 +12,32 @@ namespace KG_lab_1
 {
     public partial class MainForm : Form
     {
-        Point[,] linesArr = new Point[2, 0];
+        Point[,] linesArr; //Массив со всеми линиями
+        Point[,] currentLine; //Выбранная линия
+        Bitmap map; //холст
 
         public MainForm()
         {
             InitializeComponent();
+            Init();
+        }
+
+        //Инициализация внутренних переменных
+        private void Init()
+        {
+            linesArr = new Point[0, 2];
+            currentLine = new Point[1, 2];
+            map = new Bitmap(pictureBox1.Width, pictureBox1.Height);
         }
 
         //Добавить линию в массив линий
-        private void AddLine(Point p1, Point p2)
+        private void AddLineInArr(Point p1, Point p2)
         {
             Point[,] bufArr = new Point[linesArr.GetLength(0) + 1, linesArr.GetLength(1)];
 
-            for (int i = 0; i < bufArr.GetLength(0); i++)
+            for (int i = 0; i < linesArr.GetLength(0); i++)
             {
-                for(int j = 0; j < bufArr.GetLength(1); i++)
+                for(int j = 0; j < linesArr.GetLength(1); j++)
                 {
                     bufArr[i, j] = linesArr[i, j];
                 }
@@ -39,41 +50,48 @@ namespace KG_lab_1
         }
 
         //Получение рисунка
-        private Graphics GetGraphics(out Bitmap img)
+        private Graphics GetGraphics()
         {
             if (pictureBox1.Image == null)
             {
-                img = new Bitmap(pictureBox1.Width, pictureBox1.Height);
-                return Graphics.FromImage(img);
+                map = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                return Graphics.FromImage(map);
             }
-            else
-            {
-                img = (Bitmap)pictureBox1.Image;
-                return Graphics.FromImage(pictureBox1.Image);
-            }
+            else return Graphics.FromImage(map);
         }
 
         //Вычисление координат для точек новой линии
-
-
-        //Добавление линии на графический объект
-        private void AddLine(ref Graphics g)
+        private Point[] GetPointArr()
         {
+            Point[] line = new Point[2];
+            line[0].X = 250;
+            line[0].Y = 350;
+            line[1].X = 400;
+            line[1].Y = 300;
+
+            return line;
+        }
+
+        //Добавление линии на графический объект и добавление линии в массив линий
+        private void AddLine()
+        {
+            Graphics g = GetGraphics();
+
             Pen blackPen = new Pen(Color.Black, 3);
-            Point point1 = new Point(250, 350);
-            Point point2 = new Point(400, 300);
-            g.DrawLine(blackPen, point1, point2);
+            Point p1 = new Point(250, 350);
+            Point p2 = new Point(400, 300);
+
+            AddLineInArr(p1, p2);
+
+            g.DrawLine(blackPen, p1, p2);
         }
 
         //Создать линию
         private void CreateLine()
         {
-            Bitmap img;
-            Graphics g = GetGraphics(out img);
+            AddLine();
 
-            AddLine(ref g);
-
-            pictureBox1.Image = img;
+            pictureBox1.Image = map;
         }
 
         private void ButtonCreateLine_Click(object sender, EventArgs e)
